d33c594993a2383c207ee65af1da59b13bbfec88
kermit943 <robmei@freenet.de>
2018-04-18 22:43:16 +0200

    update to platformio project

--- AnalogUhr_mit_Neopixelring/AnalogUhr_mit_Neopixelring.ino
+++ AnalogUhr_mit_Neopixelring/AnalogUhr_mit_Neopixelring.ino
@@ -1,35 +1,32 @@
-//Analoguhr mit Neopixelring
+// Analoguhr mit Neopixelring
 
-#include <TimeLib.h>
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
 #include <FastLED.h>
+#include <TimeLib.h>
+#include <WiFiUdp.h>
 
 //#include <PubSubClient.h>
-
 
 /*---FastLED---*/
 
 #define NUM_LEDS 12
 
-#define DATA_PIN 5 // for WEMOS-D1
-//#define DATA_PIN 2 // for ESP-01
+//#define DATA_PIN 5 // for WEMOS-D1
+#define DATA_PIN 2 // for ESP-01
 CRGB leds[NUM_LEDS];
 
+const char *ssid = "QUINGDOM";
+const char *password = "DJJBXRDD";
 
-const char* ssid = "QUINGDOM";
-const char* password = "DJJBXRDD";
+// const char* mqtt_server = "192.168.0.20";
 
-//const char* mqtt_server = "192.168.0.20";
-
-
-IPAddress timeServer(130, 149, 17, 21); // time.nist.gov NTP server  ptbtime1.ptb.de
-const int timeZone = 2;     // Central European Time: 1=Winterzeit 2=Sommerzeit!!
-
-
+IPAddress timeServer(130, 149, 17,
+                     21); // time.nist.gov NTP server  ptbtime1.ptb.de
+const int timeZone = 2;   // Central European Time: 1=Winterzeit 2=Sommerzeit!!
 
 WiFiUDP Udp;
-unsigned int localPort = 8888;  // local port to listen for UDP packets
+unsigned int localPort = 8888; // local port to listen for UDP packets
 
 void setup() {
 
@@ -57,7 +54,6 @@ void setup() {
 
   cicle(4);
   Blink(2);
-
 }
 
 time_t prevDisplay = 0; // when the digital clock was displayed
@@ -65,7 +61,6 @@ time_t prevDisplay = 0; // when the digital clock was displayed
 int prev_hour;
 int prev_min;
 int min_buf;
-
 
 void loop() {
 
@@ -84,14 +79,12 @@ void loop() {
   }
 
   delay(200);
-
 }
-
 
 /*--------*/
 
-void cicle(int repeats ) {
-  for (int i = 0 ; i < repeats ; i++) {
+void cicle(int repeats) {
+  for (int i = 0; i < repeats; i++) {
     for (int dot = 0; dot < (NUM_LEDS + 1); dot++) {
       leds[dot] = CRGB::Aqua;
       FastLED.show();
@@ -103,8 +96,8 @@ void cicle(int repeats ) {
 }
 
 void Blink(int repeats) {
-  for (int i = 0 ; i < repeats ; i++) {
-    fill_solid( leds, NUM_LEDS, CRGB(150, 0, 0));
+  for (int i = 0; i < repeats; i++) {
+    fill_solid(leds, NUM_LEDS, CRGB(150, 0, 0));
     FastLED.show();
     delay(100);
     FastLED.clear();
@@ -130,25 +123,26 @@ void showtime() {
 
   if (m == h) {
     FastLED.clear();
-    leds[h] = CRGB(40, 40 , 0);
+    leds[h] = CRGB(40, 40, 0);
     // leds [m] = CRGB(40, 0, 40);
     FastLED.show();
-  }
-  else {
+  } else {
     FastLED.clear();
-    leds[h] = CRGB(0, 40 , 40);
-    leds [m] = CRGB(40, 0, 40);
+    leds[h] = CRGB(0, 40, 40);
+    leds[m] = CRGB(40, 0, 40);
     FastLED.show();
   }
 }
+
 /*-------- NTP code ----------*/
 
 const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+byte packetBuffer[NTP_PACKET_SIZE]; // buffer to hold incoming & outgoing
+                                    // packets
 
-time_t getNtpTime()
-{
-  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+time_t getNtpTime() {
+  while (Udp.parsePacket() > 0)
+    ; // discard any previously received packets
   Serial.println("Transmit NTP Request");
   sendNTPpacket(timeServer);
   uint32_t beginWait = millis();
@@ -156,10 +150,10 @@ time_t getNtpTime()
     int size = Udp.parsePacket();
     if (size >= NTP_PACKET_SIZE) {
       Serial.println("Receive NTP Response");
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      Udp.read(packetBuffer, NTP_PACKET_SIZE); // read packet into the buffer
       unsigned long secsSince1900;
       // convert four bytes starting at location 40 to a long integer
-      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 = (unsigned long)packetBuffer[40] << 24;
       secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
       secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
       secsSince1900 |= (unsigned long)packetBuffer[43];
@@ -171,24 +165,23 @@ time_t getNtpTime()
 }
 
 // send an NTP request to the time server at the given address
-void sendNTPpacket(IPAddress &address)
-{
+void sendNTPpacket(IPAddress &address) {
   // set all bytes in the buffer to 0
   memset(packetBuffer, 0, NTP_PACKET_SIZE);
   // Initialize values needed to form NTP request
   // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  packetBuffer[0] = 0b11100011; // LI, Version, Mode
+  packetBuffer[1] = 0;          // Stratum, or type of clock
+  packetBuffer[2] = 6;          // Polling Interval
+  packetBuffer[3] = 0xEC;       // Peer Clock Precision
   // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
   // all NTP fields have been given values, now
   // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.beginPacket(address, 123); // NTP requests are to port 123
   Udp.write(packetBuffer, NTP_PACKET_SIZE);
   Udp.endPacket();
 }
